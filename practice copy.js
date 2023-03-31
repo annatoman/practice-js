a61cd4631805b54d2eae2edd25cac9ae415ef68e
Anna <annamtoman@gmail.com>
2023-03-31 10:08:02 -0500

    adds tasks doc

--- practice copy.js
+++ practice copy.js
@@ -1,68 +1,78 @@
-// // # Write a method that prints out the numbers 1 to 1000 that are divisible by 3.
-// // def print_numbers_divisible_by_three
-// //   index = 1
-// //   while index <= 1000
-// //     if index % 3 == 0
-// //       puts index
-// //     end
-// //     index += 1
-// //   end
-// // end
-// // print_numbers_divisible_by_three
-// function printNumbersDivisibleByThree() {
-//     var index = 1;
-//     while (index <= 1000) {
-//       if (index % 3 === 0) {
-//         console.log(index);
-//       }
-//       index += 1;
-//     }
-//   }
-//   printNumbersDivisibleByThree();
+// # Write a method that prints out the numbers 1 to 1000 that are divisible by 3.
+// def print_numbers_divisible_by_three
+//   index = 1
+//   while index <= 1000
+//     if index % 3 == 0
+//       puts index
+//     end
+//     index += 1
+//   end
+// end
+// print_numbers_divisible_by_three
+function printNumbersDivisibleByThree() {
+    var index = 1;
+    while (index <= 1000) {
+      if (index % 3 === 0) {
+        console.log(index);
+      }
+      index += 1;
+    }
+  }
+  printNumbersDivisibleByThree();
   
   
-//   // # Write a method that accepts an array of strings and prints out every other string.
-//   // def print_every_other_item(strings)
-//   //   index = 0
-//   //   strings.each do |string|
-//   //     if index % 2 == 0
-//   //       puts string
-//   //     end
-//   //     index += 1
-//   //   end
-//   // end
-//   // print_every_other_item(["a", "b", "c", "d", "e"])
-//   function printEveryOtherItem(strings) {
-//     var index = 0;
-//     strings.forEach(function(string) {
-//       if (index % 2 === 0) {
-//         console.log(string);
-//       }
-//       index += 1;
-//     });
-//   }
-//   printEveryOtherItem(["a", "b", "c", "d", "e"]);
+  // # Write a method that accepts an array of strings and prints out every other string.
+  // def print_every_other_item(strings)
+  //   index = 0
+  //   strings.each do |string|
+  //     if index % 2 == 0
+  //       puts string
+  //     end
+  //     index += 1
+  //   end
+  // end
+  // print_every_other_item(["a", "b", "c", "d", "e"])
+  function printEveryOtherItem(strings) {
+    var index = 0;
+    strings.forEach(function(string) {
+      if (index % 2 === 0) {
+        console.log(string);
+      }
+      index += 1;
+    });
+  }
+  printEveryOtherItem(["a", "b", "c", "d", "e"]);
   
   
-//   // # Write a method that accepts an array of numbers and returns the sum.
-//   // def compute_sum(numbers)
-//   //   sum = 0
-//   //   numbers.each do |number|
-//   //     sum += number
-//   //   end
-//   //   return sum
-//   // end
-//   // puts compute_sum([2, 4, 5])
-//   function computeSum(numbers) {
-//     var sum = 0;
-//     numbers.forEach(function(number) {
-//       sum += number;
-//     });
-//     return sum;
-//   }
-//   console.log(computeSum([2, 4, 5]));
+  // # Write a method that accepts an array of numbers and returns the sum.
+  // def compute_sum(numbers)
+  //   sum = 0
+  //   numbers.each do |number|
+  //     sum += number
+  //   end
+  //   return sum
+  // end
+  // puts compute_sum([2, 4, 5])
+  function computeSum(numbers) {
+    var sum = 0;
+    numbers.forEach(function(number) {
+      sum += number;
+    });
+    return sum;
+  }
+  console.log(computeSum([2, 4, 5]));
   
-
+  // # Start with the hash: city_populations = {chi: 2700000}
+  // # Add populations to the city_populations hash for New York City (8.4 million) and San Francisco (800,000).
+  // # The result should be: {chi: 2700000, nyc: 8400000, sf: 800000}
+  // city_populations = {chi: 2700000}
+  // city_populations[:nyc] = 8400000
+  // city_populations[:sf] = 800000
+  // p city_populations
+  var cityPopulations = {chi: 2700000};
+  cityPopulations["nyc"] = 8400000;
+  cityPopulations.sf = 800000;  // same as cityPopulations["sf"]
+  console.log(cityPopulations);
   
 //   # Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
 
@@ -78,21 +88,6 @@
 //   result
 // end
 // p select_even_items(["a", "b", "c", "d", "e", "f"])
-
-
-function selectEvenItems(strings) {
-  const result = [];
-  let index = 0;
-  strings.forEach(function(string){
-    if (index % 2 === 0){
-      result.push(string)
-    }
-    index++;
-  });
-return result
-}
-
-console.log(selectEvenItems(["a", "b", "c", "d", "e", "f"]));
 
 
 // # Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
